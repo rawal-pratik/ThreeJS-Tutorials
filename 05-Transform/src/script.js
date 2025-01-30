ee776a2f1495f444e4e6b2cc08660ae059f3837f
rawal-pratik <pratikrawal40@gmail.com>
2025-01-30 14:45:34 +0530

    Adding Transform Files

---
+++ 05-Transform/src/script.js
@@ -0,0 +1,49 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+mesh.position.x = 1;
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.set(2,1,3)
+scene.add(camera)
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+mesh.scale.set(2, 0.5, 0.5);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+console.log(mesh.position.length());
+console.log(mesh.position.distanceTo(camera.position));
